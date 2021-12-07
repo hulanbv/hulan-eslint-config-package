b9313e8065de0feb64c63bf2ef1cf41c6af95c7b
Jeffrey Lanters <jeffreylanters@me.com>
2021-12-07 11:52:07 +0100

    Allowing "_id" in "no-underscore-dangle"

--- source/eslintrc.ts
+++ source/eslintrc.ts
@@ -1648,7 +1648,7 @@ module.exports = {
     'no-underscore-dangle': [
       'error',
       {
-        allow: [],
+        allow: ['_id'],
         allowAfterThis: false,
         allowAfterSuper: false,
         enforceInMethodNames: true,
