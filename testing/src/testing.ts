66b34524284a2ca151f28cce4dd0599009dd643b
Jeffrey Lanters <jeffreylanters@me.com>
2022-01-25 14:29:21 +0100

    Added testing

---
+++ testing/src/testing.ts
@@ -0,0 +1,18 @@
+function correctFunctionName() {
+  const correctVariableName = '';
+  const isCorrectVariableName = false;
+
+  console.log({ correctVariableName, isCorrectVariableName });
+}
+
+function IncorrectFunctionName() {
+  const IncorrectVariableName = '';
+  const IsIncorrectVariableName = false;
+  const incorrectVariableName = false;
+
+  console.log({
+    IncorrectVariableName,
+    IsIncorrectVariableName,
+    incorrectVariableName,
+  });
+}
